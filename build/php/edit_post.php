fa1b5de0993eabfec8b98303b94db5938986b1bd
Mohammad <exploiter0253@gmail.com>
2024-03-04 11:11:14 +0330

    Fix edit post inc file (some changes)

--- build/php/edit_post.php
+++ build/php/edit_post.php
@@ -4,8 +4,7 @@ include_once '../../config/connection.php';
 session_start();
 $user = $_SESSION['username'];
 $select = mysqli_query($connection, "select * from log_helli where username='$user' order by radif desc limit 1");
-foreach ($select as $markforlinklogs) {
-}
+$markforlinklogs=mysqli_fetch_array($select);
 $LinkLogID = @$markforlinklogs['radif'];
 $dateforupdateloghelli = $year . '/' . $month . '/' . $day . ' ' . $hour . ':' . $min . ':' . $sec;
 $urlofthispage = $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
